d34274f897f8f3eecc892bd2801f86548e8aad2d
andrynho10 <andresamaya.06@gmail.com>
2025-10-01 16:20:07 -0300

    Terminado filtro Turnos

--- src/app/dashboard/reportes/page.tsx
+++ src/app/dashboard/reportes/page.tsx
@@ -83,6 +83,7 @@ export default function ReportesPage() {
     setFechaDesde('')
     setFechaHasta('')
     setActivoSeleccionado(undefined)
+    setTurnoSeleccionado(undefined)
     setSoloProblemas(false)
     setBusqueda('')
   }
